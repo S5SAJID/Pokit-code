c1e4d58c72cf1ba9ebf15bd34c81cf9eacee6578
unknown <sk8299148@gmail.com>
2024-04-10 00:56:30 +0500

    Fix the issue with favicon

--- src/app/editor/layout.tsx
+++ src/app/editor/layout.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
   title: 'Editor — Pokit Code Editor for Developers',
   description: `Pokit Code is the ultimate code editor, providing a seamless coding experience. Perfect for both seasoned developers and beginners, Pokit Code streamlines your coding journey with its intuitive design. Enhance your efficiency and create, edit, and debug code effortlessly. Try our HTML editor for a versatile coding experience.`,
   keywords: ['Pokit Code', 'code editor', 'fast code editor', 'intuitive code editor', 'developer tools', 'coding experience', 'efficient code editing', 'code debugging', 'coding platform', 'HTML editor'],
-  icons: ['favicon.png'],
+  icons: ['images/favicon.png'],
 }
 export default function EditorLayout({children}: {children: React.ReactNode}) {
     return (
