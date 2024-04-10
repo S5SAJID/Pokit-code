debc0c55be81fcd50ccbd84c0d1503237e7bd539
unknown <sk8299148@gmail.com>
2024-04-10 22:28:29 +0500

    Added responsiveness to editors.tsx

--- src/components/Editor/Editors.tsx
+++ src/components/Editor/Editors.tsx
@@ -1,18 +1,18 @@
 "use client";
 import CodeEditor from "./EditorComponent";
 
-export default function Editor({previewRef}: {previewRef:any}) {
-    return (
-        // FIX: CONVERT THE PROPS METHOD TO CONTEXT METHOD
-        <div className="w-full grid md:grid-cols-12 h-full overflow-y-hidden">
-        <div className="editors bg-dark px-2 gap-2 col-span-2 border border-dark grid grid-rows-3 overflow-hidden max-h-full w-full items-center">
-          <CodeEditor/>
-        </div>
-        <div className="editors preview w-full md:h-full h-[92%] hidden z-10 md:block col-span-10 bg-dark pr-2">
-          <div  className="h-full rounded-xl overflow-hidden bg-light-400 ">
-            <iframe ref={previewRef}  className="h-full w-full previewCode"></iframe>
-          </div>
+export default function Editor({ previewRef }: { previewRef: any }) {
+  return (
+    // FIX: CONVERT THE PROPS METHOD TO CONTEXT METHOD
+    <div className="w-full grid md:grid-cols-8 lg:grid-cols-12 h-full overflow-y-hidden">
+      <div className="editors bg-dark px-2 gap-2 md:col-span-2 lg:col-span-3 border border-dark grid grid-rows-3 overflow-hidden max-h-full w-full items-center">
+        <CodeEditor />
+      </div>
+      <div className="editors preview w-full md:h-full h-[92%] hidden z-10 md:block md:col-span-6 lg:col-span-9 bg-dark pr-2">
+        <div className="h-full rounded-xl overflow-hidden bg-light-400 ">
+          <iframe ref={previewRef} className="h-full w-full previewCode"></iframe>
         </div>
       </div>
-    );
+    </div>
+  );
 }
