22890d5775d8371d88048f74314c61e01b8bb002
unknown <sk8299148@gmail.com>
2024-05-17 12:16:37 +0500

    Added some additional description to types

--- src/context/CodeContextProvider.tsx
+++ src/context/CodeContextProvider.tsx
@@ -14,12 +14,33 @@ const initialCode: Code = {
 
 export const CodeContext = createContext(initialCode);
 
+/**
+ * The above type `CodeType` in TypeScript React represents a code snippet with a type, value, and an
+ * `onChanged` function.
+ * @property {string} type - The `type` property in the `CodeType` type represents the type of code,
+ * such as JavaScript, CSS, HTML, etc.
+ * @property {string} value - The `value` property in the `CodeType` type represents the actual code
+ * value, which is a string containing the code itself.
+ * @property {Function} onChanged - The `onChanged` property in the `CodeType` type is a function that
+ * can be called when the value of the code type is changed. This function is typically used to notify
+ * other parts of the code that the value has been updated and trigger any necessary actions or
+ * updates.
+ */
 export type CodeType = {
     type: string,
     value: string,
     onChanged: Function,
 }
 
+/**
+ * The `Code` type in TypeScript React consists of an array of `CodeType` elements and a function
+ * `onDownload`.
+ * @property {CodeType[]} codes - The `codes` property in the `Code` type is an array of `CodeType`
+ * elements.
+ * @property {Function} onDownload - The `onDownload` property in the `Code` type is a function that is
+ * expected to be called when a download action is triggered. It is a callback function that can be
+ * used to handle the download functionality within the context of the `Code` type.
+ */
 export type Code = {
     codes: CodeType[],
     onDownload: Function
--- src/utils/editorUtils.ts
+++ src/utils/editorUtils.ts
@@ -1,6 +1,11 @@
-
 /**
- * Copy Code to clipboard & show alert if code is empty.
+ * The `copyCode` function copies the provided value to the clipboard and displays an alert if the
+ * value is empty.
+ * @param {string} value - The `value` parameter in the `copyCode` function represents the code snippet
+ * or text that you want to copy to the clipboard.
+ * @param {string} type - The `type` parameter in the `copyCode` function represents the type of code
+ * that is being copied. It is used to provide context in the alert message that is displayed when
+ * attempting to copy empty code.
  */
 export const copyCode = (value: string, type: string) => {
     if (value.length !== 0) {
@@ -17,9 +22,16 @@ export const copyCode = (value: string, type: string) => {
         }, 1600);
     }
 }
-
 /**
- * Downlaods the whole code into html file
+ * The `downloadHtml` function creates and downloads an HTML file containing provided HTML, CSS, and
+ * JavaScript code.
+ * @param {string} html - The `html` parameter in the `downloadHtml` function represents the HTML
+ * content that you want to include in the downloaded HTML file. This can be any valid HTML code that
+ * you want to save into the file.
+ * @param {string} css - The `css` parameter in the `downloadHtml` function represents the CSS code
+ * that will be included in the `<style>` tag of the generated HTML file. This CSS code will be used to
+ * style the content of the HTML file when it is displayed in a web browser.
+ * @param {string} javaScript - JavaScript code that will be included in the downloaded HTML file.
  */
 export const downloadHtml = (html: string, css: string, javaScript: string) => {
     let newFileData = `<!DOCTYPE html>
@@ -52,7 +64,16 @@ export const downloadHtml = (html: string, css: string, javaScript: string) => {
 type ClassNameActions = "ADD" | "REMOVE";
 
 /**
- * Simplifies the add and remove classes part
+ * The `manageClassName` function simplifies the process of adding or removing a class from a specified
+ * element.
+ * @param {string} elementClass - The `elementClass` parameter in the `manageClassName` function is a
+ * string that represents the class of the HTML element you want to target for adding or removing a CSS
+ * class.
+ * @param {string} className - The `className` parameter in the `manageClassName` function refers to
+ * the CSS class name that you want to add or remove from the specified element.
+ * @param {ClassNameActions} action - The `action` parameter in the `manageClassName` function
+ * specifies whether to add or remove a class from the specified element. It can have two possible
+ * values: "ADD" to add a class or "REMOVE" to remove a class.
  */
 export const manageClassName = (elementClass: string, className: string, action: ClassNameActions) => {
     const ele = document.querySelector(elementClass);
